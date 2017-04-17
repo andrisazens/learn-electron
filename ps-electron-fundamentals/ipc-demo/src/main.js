cc78a619dc1dd3e2856a4ef95f15ab6b3a01fec5
Andris Azens <andris.azens@inbox.lv>
2017-04-17 15:37:05 +0300

    Main - renderer communication with IPC

--- ps-electron-fundamentals/ipc-demo/src/main.js
+++ ps-electron-fundamentals/ipc-demo/src/main.js
@@ -1,6 +1,8 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const countdown = require("./countdown");
+const ipc = electron.ipcMain;
 
 let mainWindow;
 
@@ -12,9 +14,18 @@ app.on("ready", _ => {
 
     mainWindow.loadURL(`file://${__dirname}/countdown.html`);
 
+    //countdown();
+
     mainWindow.on("closed", _ => {
         console.log("closed");
         mainWindow = null;
+    });
+});
+
+ipc.on("countdown-start", _ => {
+    //console.log("countdown started!");
+    countdown(count => {
+        mainWindow.webContents.send("countdown", count);
     });
 });
 
