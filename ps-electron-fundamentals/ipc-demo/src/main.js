420e2c6e6ecce260d0190f4aa5873fab979985a0
Andris Azens <andris.azens@inbox.lv>
2017-04-17 15:45:10 +0300

    Spawn multiple render windows

--- ps-electron-fundamentals/ipc-demo/src/main.js
+++ ps-electron-fundamentals/ipc-demo/src/main.js
@@ -6,26 +6,32 @@ const ipc = electron.ipcMain;
 
 let mainWindow;
 
+const windows = [];
+
 app.on("ready", _ => {
-    mainWindow = new BrowserWindow({
-        width: 400,
-        height: 400
-    });
+    [1, 2, 3].forEach(_ => {
+        let win = new BrowserWindow({
+            width: 400,
+            height: 400
+        });
 
-    mainWindow.loadURL(`file://${__dirname}/countdown.html`);
+        win.loadURL(`file://${__dirname}/countdown.html`);
 
-    //countdown();
-
-    mainWindow.on("closed", _ => {
-        console.log("closed");
-        mainWindow = null;
+        win.on("closed", _ => {
+            console.log("closed");
+            mainWindow = null;
+        });
+        windows.push(win);
     });
 });
 
 ipc.on("countdown-start", _ => {
     //console.log("countdown started!");
     countdown(count => {
-        mainWindow.webContents.send("countdown", count);
+        windows.forEach(win => {
+          win.webContents.send("countdown", count);  
+        });
+        //mainWindow.webContents.send("countdown", count);
     });
 });
 
