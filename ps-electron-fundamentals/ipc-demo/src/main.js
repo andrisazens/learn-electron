47f82464f295236ab8dae8440e9f1a941f6c9ab9
Andris Azens <andris.azens@inbox.lv>
2017-04-17 15:03:05 +0300

    Close app cleanup

--- ps-electron-fundamentals/ipc-demo/src/main.js
+++ ps-electron-fundamentals/ipc-demo/src/main.js
@@ -2,9 +2,17 @@ const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-app.on("ready", _ => {    
-    new BrowserWindow({
+let mainWindow;
+
+app.on("ready", _ => {
+    mainWindow = new BrowserWindow({
         width: 400,
         height: 400
-    })
+    });
+
+    mainWindow.on("closed", _ => {
+        console.log("closed");
+        mainWindow = null;
+    });
 });
+
